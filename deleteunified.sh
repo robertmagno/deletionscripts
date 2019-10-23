794fd422cc8e313775360f49f3956586f079ffca
Rob Magno <41025018+robertmagno@users.noreply.github.com>
2019-10-23 15:15:25 -0400

    Update deleteunified.sh

--- deleteunified.sh
+++ deleteunified.sh
@@ -1,4 +1,4 @@
-kubectl delete serviceaccount cpx -n unified
 kubectl delete namespace unified
+kubectl delete serviceaccount cpx -n unified
 kubectl delete clusterrole cpx
 kubectl delete clusterrolebinding cpx
